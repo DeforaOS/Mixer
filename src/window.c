36ae13432fe605785897f37392ae0be7575dd0b5
Pierre Pronchery <khorben@defora.org>
2016-08-07 15:55:19 +0200

    Update the copyright notice

--- src/window.c
+++ src/window.c
@@ -1,7 +1,7 @@
 /* $Id$ */
 /* Copyright (c) 2015 Pierre Pronchery <khorben@defora.org> */
 static char _copyright[] =
-"Copyright © 2009-2015 Pierre Pronchery <khorben@defora.org>";
+"Copyright © 2009-2016 Pierre Pronchery <khorben@defora.org>";
 /* This file is part of DeforaOS Desktop Mixer */
 static char _license[] =
 "This program is free software: you can redistribute it and/or modify\n"
